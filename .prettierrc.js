6421e7d7e58a020ce4baf229d8bb10e045db97b3
Vladimir Perfilev <persoff68@gmail.com>
2022-02-07 23:21:29 +0100

    TDM-128 edit prettier configuration

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,10 @@
 module.exports = {
+  printWidth: 120,
   bracketSpacing: false,
   jsxBracketSameLine: true,
   singleQuote: true,
+  jsxSingleQuote: false,
+  tabWidth: 2,
   trailingComma: 'all',
-  arrowParens: 'avoid',
+  arrowParens: 'always',
 };
