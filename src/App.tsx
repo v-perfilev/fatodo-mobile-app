0fe60b727f0a737b2cf2dbdb1ed539ab7f2979d2
Vladimir Perfilev <persoff68@gmail.com>
2022-02-14 22:02:49 +0100

    TDM-129 install react navigation drawer dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React, {FC} from 'react';
-import {flowRight} from 'lodash';
-import withNavigationContainer from './shared/hocs/withNavigationContainer';
-import withStore from './shared/hocs/withStore';
-
 import './shared/i18n';
 import './shared/axios';
-import withAuthState from './shared/hocs/withAuthState';
+
+import React, {FC} from 'react';
+import {flowRight} from 'lodash';
 import {ReduxAuthState} from './store/rerducers/AuthReducer';
+import withNavigationContainer from './shared/hocs/withNavigationContainer';
+import withStore from './shared/hocs/withStore';
+import withAuthState from './shared/hocs/withAuthState';
 import RootNavigator from './navigators/RootNavigator';
 import AuthNavigator from './navigators/AuthNavigator';
 import withNativeBase from './shared/hocs/withNativeBase';
