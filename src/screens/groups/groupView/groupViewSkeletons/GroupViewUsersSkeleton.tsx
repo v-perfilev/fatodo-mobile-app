21b93935b8ef74d90c9ac50f58a22fcf6901d8d9
Vladimir Perfilev <persoff68@gmail.com>
2022-05-13 21:20:14 +0200

    TDM-135 fix user skeleton height in group view

--- src/screens/groups/groupView/groupViewSkeletons/GroupViewUsersSkeleton.tsx
+++ src/screens/groups/groupView/groupViewSkeletons/GroupViewUsersSkeleton.tsx
@@ -4,9 +4,9 @@ import {Skeleton} from 'native-base';
 const GroupViewUsersSkeleton = () => {
   return (
     <>
-      <Skeleton w="80px" h="28px" rounded="md" mr="1.5" mb="1.5" />
-      <Skeleton w="80px" h="28px" rounded="md" mr="1.5" mb="1.5" />
-      <Skeleton w="80px" h="28px" rounded="md" mr="1.5" mb="1.5" />
+      <Skeleton w="80px" h="30px" rounded="md" mr="1.5" mb="1.5" />
+      <Skeleton w="80px" h="30px" rounded="md" mr="1.5" mb="1.5" />
+      <Skeleton w="80px" h="30px" rounded="md" mr="1.5" mb="1.5" />
     </>
   );
 };
