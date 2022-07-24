63ccb44ec0ca66b43583d5ce7464fba35fc932fd
Vladimir Perfilev <persoff68@gmail.com>
2022-07-24 21:51:52 +0200

    TDM-150 edit reminder events

--- src/screens/events/eventList/eventListItem/reminder/EventReminder.tsx
+++ src/screens/events/eventList/eventListItem/reminder/EventReminder.tsx
@@ -1,8 +1,26 @@
-import {Box} from 'native-base';
+import {Text} from 'native-base';
 import withEventReminder, {WithEventReminderProps} from '../../../../../shared/hocs/withEvents/withEventReminder';
+import {Trans, useTranslation} from 'react-i18next';
+import React, {ReactElement} from 'react';
+import GroupLink from '../../../../../components/links/GroupLink';
+import ItemLink from '../../../../../components/links/ItemLink';
+import EventListItemTemplate from '../EventListItemTemplate';
 
-const EventReminder = ({group}: WithEventReminderProps) => {
-  return <Box>{group.title}</Box>;
+const EventReminder = ({group, item, date}: WithEventReminderProps) => {
+  const {t} = useTranslation();
+
+  const title = t('event:reminder.title');
+
+  const Group = (): ReactElement => (group ? <GroupLink group={group} /> : null);
+  const Item = (): ReactElement => (item ? <ItemLink item={item} /> : null);
+
+  let content = (
+    <Text>
+      <Trans i18nKey="event:reminder.content" components={{group: <Group />, item: <Item />}} />
+    </Text>
+  );
+
+  return <EventListItemTemplate title={title} content={content} date={date} />;
 };
 
 export default withEventReminder(EventReminder);
