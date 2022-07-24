5e665633c4bcc738cb68f56a5d0d302e1f153906
Vladimir Perfilev <persoff68@gmail.com>
2022-07-24 21:33:33 +0200

    TDM-150 edit default events

--- src/screens/events/eventList/eventListItem/default/EventWelcome.tsx
+++ src/screens/events/eventList/eventListItem/default/EventWelcome.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
-import {Box} from 'native-base';
+import {Text} from 'native-base';
+import {Trans, useTranslation} from 'react-i18next';
+import EventListItemTemplate from '../EventListItemTemplate';
+import {Event} from '../../../../../models/Event';
 
-const EventWelcome = () => {
-  return <Box />;
+type EventWelcomeProps = {
+  event: Event;
+};
+
+const EventWelcome = ({event}: EventWelcomeProps) => {
+  const {t} = useTranslation();
+  const date = event.createdAt;
+
+  const title = t('event:default.welcome.title');
+
+  let content = (
+    <Text>
+      <Trans i18nKey="event:default.welcome.content" />
+    </Text>
+  );
+
+  return <EventListItemTemplate title={title} content={content} date={date} />;
 };
 
 export default EventWelcome;
