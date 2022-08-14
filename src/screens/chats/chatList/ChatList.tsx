56a844f01d8a188c93e7e3f87c723c3c064b5cd0
Vladimir Perfilev <persoff68@gmail.com>
2022-08-14 14:32:57 +0200

    TDM-152 collapse header in chat list

--- src/screens/chats/chatList/ChatList.tsx
+++ src/screens/chats/chatList/ChatList.tsx
@@ -1,29 +1,124 @@
-import React, {useEffect, useState} from 'react';
+import React, {ReactElement, useCallback, useEffect, useRef, useState} from 'react';
+import {HEADER_HEIGHT} from '../../../constants';
 import ChatListControl from './ChatListControl';
-import ChatListRegular from './ChatListRegular';
-import ChatListFiltered from './ChatListFiltered';
+import CollapsableHeaderContainer, {
+  CollapsableHeaderChildrenProps,
+} from '../../../components/layouts/CollapsableHeaderContainer';
 import Header from '../../../components/layouts/Header';
+import ConditionalSpinner from '../../../components/surfaces/ConditionalSpinner';
+import FlatList, {FlatListType} from '../../../components/surfaces/FlatList';
+import {ListUtils} from '../../../shared/utils/ListUtils';
+import ChatListStub from './ChatListStub';
 import ChatListCorner from './ChatListCorner';
+import ScrollCornerButton from '../../../components/controls/ScrollCornerButton';
+import {useDelayedState} from '../../../shared/hooks/useDelayedState';
+import {Chat} from '../../../models/Chat';
+import {LayoutChangeEvent} from 'react-native';
+import {Box, useTheme} from 'native-base';
+import ChatListItem from './ChatListItem';
+import {useAppDispatch, useAppSelector} from '../../../store/store';
+import ChatsSelectors from '../../../store/chats/chatsSelectors';
+import {ChatsThunks} from '../../../store/chats/chatsActions';
 
 type ControlType = 'regular' | 'filtered';
 
 const ChatList = () => {
+  const dispatch = useAppDispatch();
+  const theme = useTheme();
+  const chats = useAppSelector(ChatsSelectors.chats);
+  const filteredChats = useAppSelector(ChatsSelectors.filteredChats);
   const [type, setType] = useState<ControlType>('regular');
   const [filter, setFilter] = useState<string>('');
+  const [loading, setLoading] = useDelayedState();
+  const [hideScroll, setHideScroll] = useState<boolean>(true);
+  const listRef = useRef<FlatListType>();
+  const [loadCounter, setLoadCounter] = useState<number>(1);
+
+  /*
+  loaders
+   */
+
+  const load = async (): Promise<void> => {
+    await dispatch(ChatsThunks.fetchChats(chats.length));
+  };
+
+  const refresh = async (): Promise<void> => {
+    await dispatch(ChatsThunks.refreshChats());
+  };
+
+  const loadFiltered = async (): Promise<void> => {
+    await dispatch(ChatsThunks.fetchFilteredChats(filter));
+  };
+
+  /*
+  keyExtractor and renderItem
+   */
+
+  const keyExtractor = useCallback((chat: Chat): string => chat.id, []);
+  const renderItem = useCallback(
+    (chat: Chat, onLayout: (event: LayoutChangeEvent) => void): ReactElement => (
+      <Box onLayout={onLayout} style={ListUtils.themedItemStyle(theme)}>
+        <ChatListItem chat={chat} />
+      </Box>
+    ),
+    [],
+  );
+
+  /*
+  scroll down button
+   */
+
+  const scrollUp = useCallback((): void => {
+    setHideScroll(true);
+    listRef.current.scrollToOffset({offset: 0});
+  }, [listRef.current]);
+
+  /*
+  Effects
+   */
 
   useEffect(() => {
-    const showFiltered = filter?.trim().length > 0;
-    setType(showFiltered ? 'filtered' : 'regular');
+    load().finally(() => setLoadCounter((prevState) => prevState - 1));
+  }, []);
+
+  useEffect(() => {
+    const filterNotEmpty = filter?.trim().length > 0;
+    setType(filterNotEmpty ? 'filtered' : 'regular');
+    if (filterNotEmpty) {
+      setLoadCounter((prevState) => prevState + 1);
+      loadFiltered().finally(() => setLoadCounter((prevState) => prevState - 1));
+    }
   }, [filter]);
 
+  useEffect(() => {
+    setLoading(loadCounter > 0);
+  }, [loadCounter]);
+
   return (
-    <>
-      <Header hideGoBack />
-      <ChatListCorner />
-      <ChatListControl setFilter={setFilter} />
-      {type === 'regular' && <ChatListRegular />}
-      {type === 'filtered' && <ChatListFiltered filter={filter} />}
-    </>
+    <CollapsableHeaderContainer header={<Header hideGoBack />}>
+      {({handleEventScroll, handleEventSnap, flatListRef}: CollapsableHeaderChildrenProps) => (
+        <>
+          <ChatListControl setFilter={setFilter} marginTop={HEADER_HEIGHT} />
+          <ConditionalSpinner loading={loading}>
+            <FlatList
+              contentContainerStyle={ListUtils.containerStyle()}
+              ListEmptyComponent={<ChatListStub />}
+              data={type === 'regular' ? chats : filteredChats}
+              render={renderItem}
+              keyExtractor={keyExtractor}
+              onScroll={handleEventScroll}
+              onMomentumScrollEnd={handleEventSnap}
+              onEndReached={type === 'regular' ? load : loadFiltered}
+              refresh={type === 'regular' ? refresh : undefined}
+              setIsOnTheTop={setHideScroll}
+              listRefs={[listRef, flatListRef]}
+            />
+          </ConditionalSpinner>
+          <ChatListCorner />
+          <ScrollCornerButton show={!hideScroll} scrollDown={scrollUp} />
+        </>
+      )}
+    </CollapsableHeaderContainer>
   );
 };
 
--- src/screens/chats/chatList/ChatListControl.tsx
+++ src/screens/chats/chatList/ChatListControl.tsx
@@ -6,13 +6,23 @@ import FBox from '../../../components/boxes/FBox';
 
 type ChatListControlProps = {
   setFilter: Dispatch<SetStateAction<string>>;
+  marginTop?: number;
 };
 
-const ChatListControl = ({setFilter}: ChatListControlProps) => {
+const ChatListControl = ({setFilter, marginTop}: ChatListControlProps) => {
   const {t} = useTranslation();
 
   return (
-    <FHStack h="50px" space="2" px="2" py="1" alignItems="center" borderBottomWidth="1" borderBottomColor="gray.200">
+    <FHStack
+      h="50px"
+      space="2"
+      px="2"
+      marginTop={marginTop}
+      py="1"
+      alignItems="center"
+      borderBottomWidth="1"
+      borderBottomColor="gray.200"
+    >
       <FBox>
         <ClearableTextInput
           h="36px"
