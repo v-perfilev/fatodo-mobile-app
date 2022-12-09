ca0c0f270ff25a8a164908e650bd3a62ba9e6236
Vladimir Perfilev <persoff68@gmail.com>
2022-12-09 20:58:40 +0100

    TDM-173 edit invertable refreshable FlatList

--- src/components/scrollable/containers/RefreshableContainer.tsx
+++ src/components/scrollable/containers/RefreshableContainer.tsx
@@ -195,7 +195,7 @@ const RefreshableContainer = ({refresh, parentScrollY, inverted, children}: Refr
       onGestureEvent={handleGestureEvent}
       onEnded={handleGestureEnded}
       onCancelled={handleGestureEnded}
-      activeOffsetY={inverted ? -10 : 10}
+      activeOffsetY={inverted ? -20 : 10}
       ref={panRef}
     >
       {content}
